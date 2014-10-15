8a6fc317c3d5ef2368d4bf569fb29b309a5b6ebf
Peter Larsen <plarsen@redhat.com>
2014-10-15 18:44:08 -0400

    Fixed connector to custom user login

--- WADE/Postgres/v0.2/config.inc.php
+++ WADE/Postgres/v0.2/config.inc.php
@@ -1,4 +1,5 @@
 <?php
-    $dsn = getenv('OPENSHIFT_POSTGRESQL_DB_URL');
+/*    $dsn = getenv('OPENSHIFT_POSTGRESQL_DB_URL'); */
+    $dsn = 'postgres://wade_app1:wade_app1@' . getenv('OPENSHIFT_POSTGRESQL_DB_HOST') . ':5432/' . getenv('OPENSHIFT_APP_HOST') . '?persist';
 ?>
 
