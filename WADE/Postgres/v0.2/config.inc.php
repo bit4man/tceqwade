c85c9d6bc7be721392d08b23d214331305d1f12d
Peter Larsen <plarsen@redhat.com>
2014-10-16 15:13:09 -0400

    Changed connection to contain db name

--- WADE/Postgres/v0.2/config.inc.php
+++ WADE/Postgres/v0.2/config.inc.php
@@ -1,5 +1,6 @@
 <?php
 /*    $dsn = getenv('OPENSHIFT_POSTGRESQL_DB_URL'); */
-    $dsn = 'postgres9://wade_app1:wade_app1@' . getenv('OPENSHIFT_POSTGRESQL_DB_HOST') . ':5432/' . getenv('OPENSHIFT_APP_HOST') . '?persist';
+    $dsn = 'postgres9://wade_app1:wade_app1@' . getenv('OPENSHIFT_POSTGRESQL_DB_HOST') . ':5432/' . getenv('OPENSHIFT_APP_HOST') . 
+           getenv('OPENSHIFT_APP_NAME') . '?persist';
 ?>
 
